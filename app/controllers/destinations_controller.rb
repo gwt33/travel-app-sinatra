033e80787b446693935b1b6946d070b7a0b1bb5a
93geraldtee <93geraldtee@gmail.com>
2020-02-27 08:41:33 -0500

    add get request for index and new, post for new

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,27 +2,34 @@ class DestinationsController < ApplicationController
 
   # GET: /destinations
   get "/destinations" do
-    erb :"/destinations/index.html"
+    @destinations = Destination.all
+    erb :"/destinations/index"
   end
 
   # GET: /destinations/new
   get "/destinations/new" do
-    erb :"/destinations/new.html"
+    erb :"/destinations/new"
   end
 
   # POST: /destinations
   post "/destinations" do
-    redirect "/destinations"
+    @destination = Destination.new(params["destination"])
+    if @destination.save
+      redirect "/destinations"
+    else
+      @errors = @destination.error.full_messages
+      erb :"/destination/new"
+    end
   end
 
   # GET: /destinations/5
   get "/destinations/:id" do
-    erb :"/destinations/show.html"
+    erb :"/destinations/show"
   end
 
   # GET: /destinations/5/edit
   get "/destinations/:id/edit" do
-    erb :"/destinations/edit.html"
+    erb :"/destinations/edit"
   end
 
   # PATCH: /destinations/5
