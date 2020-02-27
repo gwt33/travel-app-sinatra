16ee79ff08a20c973e27f4245c5bb04649895a2d
93geraldtee <93geraldtee@gmail.com>
2020-02-26 21:54:17 -0500

    add username and password_digest to users table

--- db/migrate/20200227024938_create_destinations.rb
+++ db/migrate/20200227024938_create_destinations.rb
@@ -1,7 +1,10 @@
 class CreateDestinations < ActiveRecord::Migration
   def change
     create_table :destinations do |t|
-
+      t.string :destination
+      t.string :content
+      t.integer :user_id
+      
       t.timestamps null: false
     end
   end
