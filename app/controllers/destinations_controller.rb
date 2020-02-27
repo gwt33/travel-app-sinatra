6d29f7224506562e3d4db1ad85c5111124b1d7d8
93geraldtee <93geraldtee@gmail.com>
2020-02-27 10:41:52 -0500

    add pseudocode and notes

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,17 +1,20 @@
 class DestinationsController < ApplicationController
 
   # GET: /destinations
+  # READ or index route/action
   get "/destinations" do
     @destinations = Destination.all
     erb :"/destinations/index"
   end
 
   # GET: /destinations/new
+  # New action ends in a render form
   get "/destinations/new" do
     erb :"/destinations/new"
   end
 
   # POST: /destinations
+  # CREATE action for destinations
   post "/destinations" do
     @destination = Destination.new(params["destination"])
     if @destination.save
@@ -23,18 +26,23 @@ class DestinationsController < ApplicationController
   end
 
   # GET: /destinations/5
+  # destination show action
+  # dynamic route variable (:id)
+  # params carries information
   get "/destinations/:id" do
     @destination = Destination.find_by_id(params[:id])
     erb :"/destinations/show"
   end
 
   # GET: /destinations/5/edit
+  # destinations edit action
   get "/destinations/:id/edit" do
     @destination = Destination.find_by_id(params[:id])
     erb :"/destinations/edit"
   end
 
   # PATCH: /destinations/5
+  # destinations update action
   patch "/destinations/:id" do
     @destination = Destination.find_by_id(params[:id])
     if @destination.update(params[:destination])
@@ -53,3 +61,6 @@ class DestinationsController < ApplicationController
       redirect '/destinations'
   # end
 end
+
+# get request renders a page to show something
+# patch, post delete ends in a redirect or ONLY shows errors
