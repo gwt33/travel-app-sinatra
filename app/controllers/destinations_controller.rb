c5fa2a516cee048a9db04b24ebf0e6e17d7837a8
93geraldtee <93geraldtee@gmail.com>
2020-02-27 09:42:01 -0500

    add CRUD actions for destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -24,21 +24,32 @@ class DestinationsController < ApplicationController
 
   # GET: /destinations/5
   get "/destinations/:id" do
+    @destination = Destination.find_by_id(params[:id])
     erb :"/destinations/show"
   end
 
   # GET: /destinations/5/edit
   get "/destinations/:id/edit" do
+    @destination = Destination.find_by_id(params[:id])
     erb :"/destinations/edit"
   end
 
   # PATCH: /destinations/5
   patch "/destinations/:id" do
-    redirect "/destinations/:id"
+    @destination = Destination.find_by_id(params[:id])
+    if @destination.update(params[:destination])
+      redirect "/destinations/#{@destination.id}"
+    else
+      erb :'/authors/edit'
+    end
   end
 
   # DELETE: /destinations/5/delete
   delete "/destinations/:id/delete" do
-    redirect "/destinations"
-  end
+    @destination = Destination.find_by_id(params[:id])
+    if @destination
+      @destination.destroy 
+    end
+      redirect '/destinations'
+  # end
 end
