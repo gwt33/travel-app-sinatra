cf68f45294b8e439ac8592c32aa88783b98ee9bf
93geraldtee <93geraldtee@gmail.com>
2020-02-28 16:12:59 -0500

    fixed syntax for session_secret and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,16 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :sessions_secret, "geralds-travel-app"
+    set :session_secret, "geraldstravelapp"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  post '/logout' do
+    session.clear
+    redirect '/'
   end
 
   helpers do
@@ -20,7 +25,7 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       User.find_by(id: session[:user_id])
+      # @current_user ||= User.find(username: session[:username]) if session[:username]
     end
   end
-
 end
